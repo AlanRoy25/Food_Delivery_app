3697f007714e391ae5002ffafa00c2afbe73c8ff
AlanRoy25 <alanroythomas4@gmail.com>
2024-08-10 02:29:14 +0530

    navbar done with css

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import './Navbar.css'
+import { assets } from '../../assets/assets'
+
+const Navbar = ({cartItemCount}) => {
+
+  const [menu, setMenu] = useState("home")
+  return (
+    <div className='navbar'>
+      <img src={assets.logo} alt="" className='logo' />
+     <ul className="navbar-menu">
+      <li onClick={() => setMenu("home")} className={menu === "home" ? "active" : ""}>Home</li>
+      <li onClick={() => setMenu("menu")}className={menu === "menu" ? "active" : ""}>Menu</li>
+      <li onClick={() => setMenu("mobile-app")}className={menu === "mobile-app" ? "active" : ""}>mobile-app</li>
+      <li onClick={() => setMenu("contact-us")}className={menu === "contact-us"? "active" : ""}>contact us </li>
+     </ul>
+     <div className='navbar-right'>
+      <img src={assets.search_icon} alt=""  />
+      <div className="navbar-search-icon">
+        <div className="cart-container">
+        <img src={assets.basket_icon} alt="" className='cart-icon' />
+        {cartItemCount > 0 && <span className="cart-count">{cartItemCount}</span>}
+        </div>
+        <div className="dot">
+          <button >Sign in</button>
+        </div>
+      </div>
+
+     </div>
+    </div>
+  )
+}
+
+export default Navbar
