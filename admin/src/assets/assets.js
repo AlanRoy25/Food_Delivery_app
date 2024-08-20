ea1f669001f55656c7f4ed0e8c5b207a8a8aa5e5
AlanRoy25 <alanroythomas4@gmail.com>
2024-08-20 21:01:58 +0530

    created admin dashboard

---
+++ admin/src/assets/assets.js
@@ -0,0 +1,16 @@
+import logo from './Swiggy-emblem.png'
+import addicon from './add_icon_white.png'
+import ordericon from  './ordericon.png'
+import profileimage from './profile_icon.png'
+import uploadarea from './uploadicon.jpg'
+import parcelicon from './parcel_icon.png'
+
+
+export const assets = {
+  logo,
+  addicon,
+  ordericon,
+  profileimage,
+  uploadarea,
+  parcelicon
+}
---
+++ admin/src/main.jsx
@@ -0,0 +1,12 @@
+
+import {BrowserRouter} from 'react-router-dom'
+import App from './App.jsx'
+import './index.css'
+import ReactDOM from 'react-dom/client'
+import React from 'react'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+)
