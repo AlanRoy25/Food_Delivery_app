a13931c2b6225a1510839e1fc64b35da0c1512b6
Zuriel <alanroythomas4@gmail.com>
2024-08-22 13:00:40 +0530

    changes in backend

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,6 +1,13 @@
 import mongoose from "mongoose";
+import 'dotenv/config';
 
- export const connectdb = async() => {
-  await mongoose.connect('mongodb+srv://alanroy:Qazwsxedc0225!@delivery-app.lvrbx.mongodb.net/Delivery-app').then(()=>console.log('DB Connected'));
-
-}
+export const connectdb = async () => {
+  try {
+    await mongoose
+      .connect(process.env.MONGODB_SECRET)
+      .then(() => console.log("DB Connected"));
+  } catch (error) {
+    console.error("DB connection error:", error);
+    process.exit(1);
+  }
+};
--- Backend/server.js
+++ Backend/server.js
@@ -8,12 +8,13 @@ import userRouter from "./routes/userRoutes.js";
 // app config
 
 const app = express();
-const port = 4000
+
+
 
 //middlewares
 
 app.use(express.json())
-app.use(cors())
+
 
 // db connection
 
@@ -30,9 +31,9 @@ app.get('/' ,(req,res) => {
   res.send('Api working')
 })
 
-app.listen(port, ()=> {
-  console.log(`Server Started on https://localhost:${port}`);
-  
-})
+const PORT = process.env.PORT || 4000
+
+app.listen(PORT, ()=> 
+  console.log(`Server Started on ${PORT}`));
 
 //mongodb+srv://alanroy:Qazwsxedc0225!@delivery-app.lvrbx.mongodb.net/
