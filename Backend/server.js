e555791b46666e3fa81bd98afd648c7b3deef468
Zuriel <alanroythomas4@gmail.com>
2024-08-22 16:48:35 +0530

    added package

--- Backend/server.js
+++ Backend/server.js
@@ -1,38 +1,28 @@
-import express from "express"
+import express from "express";
 import { connectdb } from "./config/db.js";
 import FoodRouter from "./routes/foodroute.js";
-import 'dotenv/config'
-import userRouter from "./routes/userRoutes.js";
+import 'dotenv/config';
+import userRouter from "./routes/userRoutes.js"; // Corrected path
 
 // app config
-
 const app = express();
 
-
-
-//middlewares
-
-app.use(express.json())
-
+// middlewares
+app.use(express.json());
 
 // db connection
-
-connectdb()
+connectdb();
 
 // api endpoint
+app.use('/api/food', FoodRouter);
+app.use('/images', express.static('uploads'));
+app.use('/api/user', userRouter);
 
-app.use('/api/food', FoodRouter)
-app.use('/images', express.static('uploads'))
-app.use('/api/user', userRouter)
+app.get('/', (req, res) => {
+  res.send('Api working');
+});
 
+const PORT = process.env.PORT || 4000;
 
-app.get('/' ,(req,res) => {
-  res.send('Api working')
-})
-
-const PORT = process.env.PORT || 4000
-
-app.listen(PORT, ()=> 
+app.listen(PORT, () => 
   console.log(`Server Started on ${PORT}`));
-
-//mongodb+srv://alanroy:Qazwsxedc0225!@delivery-app.lvrbx.mongodb.net/
